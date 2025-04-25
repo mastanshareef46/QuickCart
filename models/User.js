b91a8c042336beeef2425ae8e622096157bec1bc
mastanshareef46 <shaikmastanshareef15@gmail.com>
2025-04-25 17:01:03 +0530

    commit

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     _id: { type: String, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    Imageurl: { type: String},
+    Imageurl: { type: String ,required :true},
     cartItems: { type: Map, of: mongoose.Schema.Types.Mixed, default: {} }, 
   },
   { minimize: false }
